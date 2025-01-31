f2ac502b0729cf0ef29ca10a29ffe5254cd04fbc
Alwi842 <alwi.abdullah1221@gmail.com>
2025-01-31 13:53:05 +0700

    update swr

--- src/pages/products/[id]/index.js
+++ src/pages/products/[id]/index.js
@@ -16,36 +16,45 @@ import useSWR from "swr";
 const ProductDetailPage = ({ detailProduct }) => {
   const router = useRouter();
   const api = process.env.NEXT_PUBLIC_API;
-  const { data } = useSWR(
+  const { data, error, isLoading, isValidating } = useSWR(
     `${api}/products/${detailProduct?.id}`,
     async () => {
       const res = await axios.get(`${api}/products/${detailProduct?.id}`);
       return res.data;
     },
-    { initialData: detailProduct }
+    { initialData: detailProduct, refreshInterval: 10000 }
   );
+
+  if (error) return <div className="h-screen text-8xl text-center">Gagal ambil data</div>;
   return (
     <div className="flex flex-col px-6 py-8 min-h-screen bg-gradient-to-b from-black to-blue-900">
       {/* Title */}
-      <h1 className="text-4xl font-extrabold text-white">Detail Produk</h1>
+      {isLoading ? (
+        <h1 className="text-4xl font-extrabold text-white">Loading data...</h1>
+      ) : (
+        <>
+          <h1 className="text-4xl font-extrabold text-white">Detail Produk</h1>
 
-      {/* Product Details */}
-      <div className="mt-6 p-6 bg-white bg-opacity-20 backdrop-blur-md rounded-2xl max-w-xl shadow-lg">
-        <h2 className="text-2xl font-bold text-white">{data?.title}</h2>
-        <p className="mt-4 text-white text-lg">{data?.description}</p>
-        <p className="mt-5 text-white text-2xl font-bold">{formatCurrency(data?.price)}</p>
+          {/* Product Details */}
+          <div className="mt-6 p-6 bg-white bg-opacity-20 backdrop-blur-md rounded-2xl max-w-xl shadow-lg">
+            <h2 className="text-2xl font-bold text-white">{data?.title}</h2>
+            <p className="mt-4 text-white text-lg">{data?.description}</p>
+            <p className="mt-5 text-white text-2xl font-bold">{formatCurrency(data?.price)}</p>
 
-        {/* Buttons Section */}
-        <div className="mt-6 flex gap-4">
-          {/* Back Button */}
-          <button
-            onClick={() => router.back()}
-            className="px-6 py-3 text-lg font-semibold text-white bg-gray-700 rounded-lg hover:bg-gray-600 transition-all"
-          >
-            ← Kembali
-          </button>
-        </div>
-      </div>
+            {/* Buttons Section */}
+            <div className="mt-6 flex gap-4">
+              {/* Back Button */}
+              <button
+                onClick={() => router.back()}
+                className="px-6 py-3 text-lg font-semibold text-white bg-gray-700 rounded-lg hover:bg-gray-600 transition-all"
+              >
+                ← Kembali
+              </button>
+            </div>
+          </div>
+          {isValidating && <h1 className="text-4xl font-extrabold text-white">Updating data...</h1>}
+        </>
+      )}
     </div>
   );
 };
